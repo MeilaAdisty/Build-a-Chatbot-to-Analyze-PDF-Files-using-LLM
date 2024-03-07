ab4c164370194448cd362d22fde3bc134f601711
Meila Adisty Putri <160561377+MeilaAdisty@users.noreply.github.com>
2024-03-07 05:08:11 +0000

    Commit Ketujuh

--- worker.py
+++ worker.py
@@ -23,7 +23,7 @@ llm_embeddings = None
 def init_llm():
     global llm, llm_embeddings
     # Initialize the language model with the OpenAI API key
-    api_key="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"
+    api_key=""
     llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=api_key)
     # Initialize the embeddings for the language model
     llm_embeddings = OpenAIEmbeddings(openai_api_key = api_key)
